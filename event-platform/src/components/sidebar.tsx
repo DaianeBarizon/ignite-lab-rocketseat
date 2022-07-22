0a1eee7ab6aefc7d33dd477e1b0036b5c9ff6a90
daianebarizon <daianebarizon22@gmail.com>
2022-07-22 20:39:59 -0300

    feat: add loading in the sidebar component

--- event-platform/src/components/sidebar.tsx
+++ event-platform/src/components/sidebar.tsx
@@ -26,6 +26,10 @@ type GetLessonQueryResponse = {
 export function Sidebar() {
   const { data } = useQuery<GetLessonQueryResponse>(GET_LESSON_QUERY);
 
+  if (!data) {
+    return <p>carregando...</p>;
+  }
+
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">Cronogramas das aulas</span>
